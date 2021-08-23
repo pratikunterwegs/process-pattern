d64757108ae166703c4ccfd04c076481bf383c82
Pratik Gupte <pratikgupte16@gmail.com>
2021-08-23 12:30:19 +0200

    correct coordinates for 0 index + add summary fun

--- R/fun_link_landscape.R
+++ R/fun_link_landscape.R
@@ -25,9 +25,10 @@ read_landscape <- function(landscape_file, layer = 1,
   )
   
   # make x and y numeric
+  # subtract one because positions start at 0
   land[, c("x", "y") := list(
-    as.numeric(x),
-    as.numeric(y)
+    as.numeric(x) - 1, 
+    as.numeric(y) - 1
   )]
   # return
   land
@@ -48,4 +49,40 @@ link_landscape = function(pos_data, land_data) {
     all.x = TRUE,
     all = F
   )
+}
+
+#' Get summary stats on the landscape visited by the agent.
+#'
+#' @param pos_data Agent position data with x and y cols.
+#' @param land_data Landscape data with x and y cols, needs one other col
+#' at minimum to give summary stats.
+#'
+#' @return A data.table of mean and sd of the landscape variables.
+#' @export
+get_path_env_summary = function(pos_data, land_data) {
+  
+  # check landscape
+  assertthat::assert_that(
+    length(colnames(land_data)) >= 3
+  )
+  
+  # get locations visited by agent
+  tmp_land_ = data.table::merge.data.table(
+    land_data, 
+    pos_data[, c("x", "y")], by = c("x", "y")
+  )
+  
+  # melt by cell id to get long df
+  tmp_land_ = data.table::melt(tmp_land_, id.vars = c("x", "y"))
+  
+  # summarise landscape data agnostic to which are present
+  land_summary = tmp_land_[, unlist(lapply(.SD, function(v) {
+    list(
+      mean = mean(v, na.rm = T),
+      sd = sd(v, na.rm = T)
+    )
+  }), recursive = FALSE), by = c("variable"), .SDcols = "value"]
+  
+  # return land summary
+  land_summary
 }
