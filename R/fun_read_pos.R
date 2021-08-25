f8e5714b133e0456488c142543e5d84cf0209023
Pratik Gupte <pratikgupte16@gmail.com>
2021-08-25 12:30:47 +0200

    fun to read pos

---
+++ R/fun_read_pos.R
@@ -0,0 +1,9 @@
+
+read_data = function(path = ".", gen_start = 50, gen_end = 250, gen_step = 10) {
+  # list generation files
+  files = glue("data/{path}/{str_pad(seq({gen_start}, {gen_end}, {gen_step}),\\
+               5, '0', side = 'left')}_pos.csv")
+  
+  data = lapply(files, data.table::fread)
+  data = data.table::rbindlist(data)
+}
