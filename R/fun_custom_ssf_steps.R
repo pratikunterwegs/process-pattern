bda3d1b01b29464fb17d267b2eaaa01d038abf54
Pratik Gupte <pratikgupte16@gmail.com>
2021-08-25 14:37:10 +0200

    fun prep ssf

--- R/fun_custom_ssf_steps.R
+++ R/fun_custom_ssf_steps.R
@@ -13,7 +13,7 @@
 #' @return A data.table of alternate steps.
 #' @export
 #'
-pick_alt_steps = function(x, y, x2, y2, landsize, 
+pick_alt_steps = function(x, y, x2, y2, landsize = 512, 
                           chebyshev_distance = 1, n_samples = 8) {
   
   assertthat::assert_that(
@@ -38,5 +38,33 @@ pick_alt_steps = function(x, y, x2, y2, landsize,
     steps = steps[sample.int(n = nrow(steps), size = n_samples, replace = F),]
   }
   
+  # bind real steps and mark case
+  steps = rbind(steps, list(x2, y2))
+  steps$case_ = c(rep(FALSE, n_samples), TRUE)
+  
   steps
+}
+
+prep_path_ssf = function(data, landsize = 512,
+                         chebyshev_distance = 1, n_samples = 8) {
+  # get x2 y2
+  data[, `:=`(
+    x2 = data.table::shift(x, type = "lead"),
+    y2 = data.table::shift(y, type = "lead")
+  )]
+  # remove last as no known end
+  data = data[complete.cases(data),]
+  
+  # select alt steps per coord
+  alt_steps = Map(pick_alt_steps, data$x, data$y, data$x2, data$y2, 
+      landsize = landsize, chebyshev_distance = chebyshev_distance,
+      n_samples = n_samples)
+  
+  # assign step id
+  alt_steps = Map(function(df, sid_) {
+    df[, step_id_ := sid_]
+  }, alt_steps, seq_len(length(alt_steps)))
+  
+  # bind list and return
+  data.table::rbindlist(alt_steps)
 }
