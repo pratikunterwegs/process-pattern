163c60fc9d5971c04e6d3f8317c8cfc3652f3a9c
Pratik Gupte <pratikgupte16@gmail.com>
2021-08-26 11:47:13 +0200

    edit path

--- R/fun_read_pos.R
+++ R/fun_read_pos.R
@@ -1,7 +1,7 @@
 
 read_data = function(path = ".", gen_start = 50, gen_end = 250, gen_step = 10) {
   # list generation files
-  files = glue("data/{path}/{str_pad(seq({gen_start}, {gen_end}, {gen_step}),\\
+  files = glue("{path}/{str_pad(seq({gen_start}, {gen_end}, {gen_step}),\\
                5, '0', side = 'left')}_pos.csv")
   
   data = lapply(files, data.table::fread)
