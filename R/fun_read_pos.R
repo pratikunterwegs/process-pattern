1aa3b540e2f45b2b7bc1da697e6571126f59c7c3
Pratik Gupte <pratikgupte16@gmail.com>
2021-08-26 15:11:27 +0200

    read last gen

--- R/fun_read_pos.R
+++ R/fun_read_pos.R
@@ -1,7 +1,8 @@
 
-read_data = function(path = ".", gen_start = 50, gen_end = 250, gen_step = 10) {
+read_data = function(path = ".", gen_start = 50, gen_end = 250, gen_step = 10,
+                     gen_add = 249) {
   # list generation files
-  files = glue("{path}/{str_pad(seq({gen_start}, {gen_end}, {gen_step}),\\
+  files = glue("{path}/{str_pad(c(seq({gen_start}, {gen_end}, {gen_step}), {gen_add}),\\
                5, '0', side = 'left')}_pos.csv")
   
   data = lapply(files, data.table::fread)
